838d397922fa54d8dbc69b75998824ffe6c360e8
amrelwekeel <amr Elwekeel>
2022-03-21 19:58:18 +0200

    create new Folders

--- test/data_structure_project_test.dart
+++ test/data_structure_project_test.dart
@@ -1,8 +1,7 @@
-import 'package:data_structure_project/data_structure_project.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    expect(calculate(), 42);
+
   });
 }
