cbf7cc64780e94efcdf2bb7a529e9c7c3c1d4fa5
amrelwekeel <amr Elwekeel>
2022-03-22 04:33:20 +0200

    linkedList

--- bin/linked_list.dart
+++ bin/linked_list.dart
@@ -1,16 +1,46 @@
+import 'package:data_structure_project/src/linked_list/linked_list.dart';
 import 'package:data_structure_project/src/nodes/node.dart';
 
 
 
-void main(){
+void main() {
 
-  final node1 = Node(value: 1);
-  final node2 = Node(value: 2);
-  final node3 = Node(value: 3);
+  final list = LinkedList<int>();
 
-  node1.next =node2 ;
-  node2.next = node3;
-  print(node1);
+
+
+
+  list.push(3);
+  list.push(2);
+  list.push(1);
+  print("Before $list");
+  var middleNode = list.nodeAt(2)!;
+   list.insertAfter(middleNode, 24);
+   print('After:' '$list');
+
+
+  //
+  // list.push(3);
+  // list.push(2);
+  // list.push(1);
+  // list.append(5);
+  // list.append(5);
+  // list.append(7);//  this function push items an end the List
+  // list.push(0);// this push items in front List
+  //
+  // print(list); // result 1 -> 2 -> 3
+
+
 
 
 }
+///{
+/// Node Part
+// final node1 = Node(value: 1);
+// final node2 = Node(value: 2);
+// final node3 = Node(value: 3);
+//
+// node1.next =node2 ;
+// node2.next = node3;
+// print(node1);
+///}
--- lib/src/linked_list/linked_list.dart
+++ lib/src/linked_list/linked_list.dart
@@ -18,6 +18,69 @@ class  LinkedList<E> {
   }
 
 
+  ///there are three way to provide an interface to manage the node
+
+// Push to FrontList
+
+   void push(E value){
+     head = Node(value: value, next: head);
+     /// this from if the list is empty will  be this node is head and tail
+     tail ??= head ;
+   }
+
+// append from if you want put items in End List
+
+   void append(E value){
+    // if this list is empty
+    if(isEmpty){
+       push(value);
+       return ;
+    }
+
+    // else
+     tail!.next = Node(value: value);
+     tail =tail!.next;
+
+   }
+
+//insertAfter this from if you want put items in middle List
+   Node<E>? nodeAt (int index){
+
+     var currentNode = head ;
+     var currentIndex = 0;
+
+     while(currentNode != null && currentIndex < index){
+       currentNode = currentNode.next;
+       currentIndex +=1 ;
+     }
+     return currentNode ;
+
+  }
+  // this Method call tail node
+  Node<E>? insertAfter(Node<E> node , E value){
+        if(tail == node){
+           append(value);
+           return tail;
+        }
+        node.next = Node(value: value, next: node.next);
+        return node.next!;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
