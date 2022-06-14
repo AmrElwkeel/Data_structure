b90d3f6194b19319fb9445dfd4fc23c5ef026169
amrelwekeel <amr Elwekeel>
2022-06-14 02:38:34 +0200

    Stack Iteratable

--- bin/main.dart
+++ bin/main.dart
@@ -2,22 +2,19 @@ import 'package:data_structure_project/src/linked_list/linked_list.dart';
 import 'package:data_structure_project/src/nodes/node.dart';
 import 'package:data_structure_project/src/stack/stack.dart';
 
-
-
 void main() {
-
-
-  final  _stack = Stack<int>();
-   _stack.push(1);
-   _stack.push(2);
-   _stack.push(3);
-
-   print(_stack);
-   final element = _stack.pop();
-   print("this is Poped Element: $element");
-
-
-
+  const list = ['S', 'M', 'O', 'K', 'E'];
+  final smokeStack = Stack.of(list);
+  print(smokeStack);
+  smokeStack.pop();
+  // final  _stack = Stack<int>();
+  //  _stack.push(1);
+  //  _stack.push(2);
+  //  _stack.push(3);
+  //
+  //  print(_stack);
+  //  final element = _stack.pop();
+  //  print("this is Poped Element: $element");
 
   // final list = LinkedList<int>();
   //
@@ -43,11 +40,8 @@ void main() {
   // list.push(0);// this push items in front List
   //
   // print(list); // result 1 -> 2 -> 3
-
-
-
-
 }
+
 ///{
 /// Node Part
 // final node1 = Node(value: 1);
--- lib/src/stack/stack.dart
+++ lib/src/stack/stack.dart
@@ -1,24 +1,21 @@
-
-
-
-
-
-
 class Stack<E> {
-  Stack() : _storage = <E>[];
   final List<E> _storage;
 
+  // Stack() : _storage = <E>[];
+  Stack.of(Iterable<E> elements) : _storage = List<E>.of(elements);
   @override
   String toString() {
     return 'Stack ______ Last\n${_storage.reversed.join("\n")}';
   }
 
-
-  void push(E element){
+  void push(E element) {
     _storage.add(element);
-
   }
 
   E pop() => _storage.removeLast();
 
+  E get peek => _storage.last;
+
+  bool get isEmpty => _storage.isEmpty;
+  bool get isNotEmpty => !isEmpty;
 }
